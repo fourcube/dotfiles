efd49768bfce919fc5dd85be704acab8e07d8c0d
Chris Grieger <chris@scolp.de>
2015-05-18 11:23:27 +0200

    Add vimrc. Update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-sudo apt-get install build-essential git-core vim
+sudo apt-get install build-essential git-core vim-nox
+ln -s ~/dotfiles/vim ~/.vim
+ln -s ~/dotfiles/vim/vimrc ~/.vimrc
